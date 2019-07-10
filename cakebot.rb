f97efa7567a846bcfe29ea0aa4a5f6e65d853638
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-07-10 21:25:27 +0200

    Add minimal bot

---
+++ cakebot.rb
@@ -0,0 +1,10 @@
+require 'dotenv/load'
+require 'slack-ruby-bot'
+
+class PongBot < SlackRubyBot::Bot
+  command 'ping' do |client, data, match|
+    client.say(text: 'pong', channel: data.channel)
+  end
+end
+
+PongBot.run
