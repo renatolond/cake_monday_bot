e93da57d654059c4c09305881af865f13787fc61
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-07-15 23:28:04 +0200

    Fix archive migration

--- db/migrate/20190715205721_create_archives.rb
+++ db/migrate/20190715205721_create_archives.rb
@@ -3,7 +3,7 @@ ROM::SQL.migration do
     create_table :archives do
       primary_key :id
       foreign_key :candidate_id, :candidates, null: false
-      column :drawn_at, :date, null: false
+      column :drawn_at, :time, null: false
       index :drawn_at
     end
   end
