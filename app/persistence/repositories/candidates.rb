4a15f0db2f7c231ec19bdf1b6ccc90410d4b5a79
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-07-15 20:33:37 +0200

    Add logic for draw, still ignoring getting out twice in a row

--- app/persistence/repositories/candidates.rb
+++ app/persistence/repositories/candidates.rb
@@ -8,6 +8,14 @@ module Persistence
       def available
         candidates.where(available: true)
       end
+
+      def reset_pool
+        candidates.command(:update).call(available: true)
+      end
+
+      def make_unavailable(user_id)
+        candidates.by_pk(user_id).command(:update).call(available: false)
+      end
     end
   end
 end
--- app/services/draw_candidate_service.rb
+++ app/services/draw_candidate_service.rb
@@ -2,12 +2,33 @@
 
 require_relative "../persistence/setup"
 
+class TooFewCandidates < StandardError; end
+
 class DrawCandidateService
   def draw
+    available_candidates = candidates_repository.available
+    if available_candidates.to_a.empty?
+      candidates_repository.reset_pool
+      available_candidates = candidates_repository.available
+    end
+
+    candidate_ids = available_candidates.map { |candidate| candidate[:id] }
+    chosen = candidate_ids.sample
+    while false && chosen == history_repository.last_selected_id
+      raise TooFewCandidates.new("Not enough candidates for sampling") if candidate_ids.size == 1
+      chosen = candidate_ids.sample
+    end
+
+    candidates_repository.make_unavailable(chosen)
+    chosen
   end
 
   private
     def candidates_repository
-      repo = Persistence::Repositories::Candidates.new(Persistence.rom)
+      @candidates_repository ||= Persistence::Repositories::Candidates.new(Persistence.rom)
+    end
+
+    def history_repository
+      nil
     end
 end
