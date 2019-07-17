24b10a9a53dcf9099ea15dfb9e6bea18a1a240a5
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-07-17 23:04:14 +0200

    Move bot code into another file

--- cakebot.rb
+++ cakebot.rb
@@ -1,14 +1,8 @@
 require "dotenv/load"
 require "slack-ruby-bot"
 
-require_relative 'config/initializers/rom'
+require_relative "config/initializers/rom"
 
-require_relative 'app/services/draw_candidate_service'
+require_relative "app/services/draw_candidate_service"
 
-class PongBot < SlackRubyBot::Bot
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
-  end
-end
-
-PongBot.run
+require_relative "app/objects/cake_bot"
