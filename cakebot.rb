725ecd6e30a17b85ba440151a754b5cc7ebcab4c
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-07-17 23:30:30 +0200

    Add slack at to message, add command to add user

--- cakebot.rb
+++ cakebot.rb
@@ -14,12 +14,17 @@ class CakeBotCommands < Thor
     candidate = DrawCandidateService.new.draw
 
     web_client = Slack::Web::Client.new(token: ENV["SLACK_API_TOKEN"])
-    web_client.chat_postMessage(text: "Hey, #{candidate.name}, you bring the cake this week!", channel: "random")
+    web_client.chat_postMessage(text: "Hey, @#{candidate.slack_at} #{candidate.name}, you bring the cake this week!", channel: "random", link_names: true)
   end
 
-  desc "add NAME", "Adds a user to the database"
-  def add(*name)
-    raise "Not implemented"
+  desc "add SLACK_AT NAME", "Adds a user to the database, pass the user @, like renatolond, and their name"
+  def add(slack_at, *name)
+    candidates_repo = Persistence::Repositories::Candidates.new(Persistence.rom)
+    name = name.join(" ")
+    candidates_repo.create(slack_at: slack_at, name: name)
+    puts "User #{name} added as @#{slack_at}"
+  rescue ROM::SQL::UniqueConstraintError
+    puts "User with @#{slack_at} already existed. Ignoring!"
   end
 
   desc "start", "Starts the bot, do not daemonize"
