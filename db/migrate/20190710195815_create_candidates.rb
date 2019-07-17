ca9c7d049d9d34b9f428e6deea8ab795d92473b8
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-07-17 23:30:13 +0200

    Fix candidate table creation, add slack at

--- db/migrate/20190710195815_create_candidates.rb
+++ db/migrate/20190710195815_create_candidates.rb
@@ -3,7 +3,9 @@ ROM::SQL.migration do
     create_table :candidates do
       primary_key :id
       column :name, :varchar, null: false
+      column :slack_at, :varchar, null: false
       column :available, :boolean, null: false, default: true
+      unique :slack_at
     end
   end
 end
